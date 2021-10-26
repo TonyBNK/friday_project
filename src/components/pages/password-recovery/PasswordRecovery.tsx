2e9d799ca9692e3c3616efa89b39d43663d85d52
TonyBNK <borisenk-anton@yandex.ru>
2021-10-26 08:51:48 +0300

    1. Added password recovery page with local state.

--- src/components/pages/password-recovery/PasswordRecovery.tsx
+++ src/components/pages/password-recovery/PasswordRecovery.tsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, {ChangeEvent, useState} from 'react'
+import {Input} from "../../common/Input/Input";
+import {Button} from "../../common/Button/Button";
 
 
 export const PasswordRecovery = () => {
+    const [email, setEmail] = useState<string>('');
+
+
+    const onEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.currentTarget.value);
+    }
+    const onButtonClick = () => {
+        alert(`email: ${email}`);
+    }
+
     return (
         <div>
-            Password Recovery
+            <h2>Forgot your password?</h2>
+            <Input
+                type="text"
+                name="email"
+                value={email}
+                onChange={onEmailChange}
+            />
+            <p>
+                Enter your email address and we will send you further instructions
+            </p>
+            <Button onClick={onButtonClick}>
+                Send instructions
+            </Button>
         </div>
     )
 }
