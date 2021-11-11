055184d3f189bf5cc4a6275fc25551a73703fe76
TonyBNK <borisenk-anton@yandex.ru>
2021-11-11 10:25:38 +0300

    3. Added Modal window.

--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -5,7 +5,9 @@ import c from "./Modal.module.scss";
 export const Modal = () => {
     return (
         <div className={c.modalContainer}>
+            <div className={c.modalWindow}>
 
+            </div>
         </div>
     );
 }
